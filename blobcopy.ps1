5b8f51a1ea52e32ab88e8afe618017998a88ae69
Prabath Anuradha <anuradha.prabath@gmail.com>
2021-08-14 09:00:23 +0530

    test

--- blobcopy.ps1
+++ blobcopy.ps1
@@ -2,3 +2,4 @@ azcopy login # Login
 
 azcopy copy 'https://fntrainingvideos.blob.core.windows.net/test2?sv=2020-04-08&st=2021-06-22T07%3A08%3A06Z&se=2021-06-23T07%3A08%3A06Z&sr=c&sp=rl&sig=sm3dBTOqW8cnMUUXSCQYOmQFNSRO0VpK7HBADifMjUE%3D' 'https://fnsite.blob.core.windows.net/test2' --recursive
 
+#endregion
